4a9627d6d630367bd1682202e9e585dbd94a810c
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-09-09 17:55:26 +0530

    setup tailwindcss and svgs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import {
+  AppLogo,
+  BookmarkLogo,
+  HomeLogo,
+  HyperlinkLogo,
+  MovieLogo,
+  SearchLogo,
+  StarFilledLogo,
+  StarLogo,
+  TmdbLogo,
+  TVSeriesLogo,
+} from "./assets/logos";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <>
-      <div style={{ textAlign: "center" }}>
-        <h1>
-          Count: <span data-testid='counter'>{count}</span>
-        </h1>
-        <button onClick={() => setCount((prev) => prev + 1)} data-testid='counter-increment'>
-          Increment
-        </button>
-      </div>
-    </>
+    <div className="p-10 bg-surface text-onSurface h-full w-full">
+      <p className="text-heading-lg">Heading large</p>
+      <p className="text-heading-md">Heading md</p>
+      <p className="text-heading-sm">Heading small</p>
+      <p className="text-heading-xs">Heading extra small</p>
+      <p className="text-body-md">Body Medium</p>
+      <p className="text-body-sm">Body small</p>
+
+      <HomeLogo className="text-primary " />
+      <MovieLogo />
+      <TVSeriesLogo />
+      <BookmarkLogo />
+      <AppLogo />
+      <SearchLogo />
+      <StarLogo className=" text-primary" />
+      <StarFilledLogo className="text-primary " />
+      <HyperlinkLogo />
+      <TmdbLogo />
+    </div>
   );
 }
 
---
+++ client/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FC4747",
+        onPrimary: "#FFFFFF",
+        secondary: "#161D2F",
+        onSecondary: "#FFFFFF",
+        tertiary: "#5A698F",
+        onTertiary: "#FFFFFF",
+        surface: "#10141E",
+        onSurface: "#FFFFFF",
+        muted: "#A1A1AA",
+      },
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
+      fontWeight: {
+        light: 300,
+        medium: 500,
+      },
+      fontSize: {
+        "heading-lg": ["2rem", { fontWeight: 300 }],
+        "heading-md": ["1.5rem", { fontWeight: 300 }],
+        "body-md": ["0.938rem", { fontWeight: 300 }],
+        "body-sm": ["0.813rem", { fontWeight: 300 }],
+        "heading-sm": ["1.5rem", { fontWeight: 500 }],
+        "heading-xs": ["1.125rem", { fontWeight: 500 }],
+      },
+    },
+  },
+  plugins: [],
+};
